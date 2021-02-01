f1bf74068002f68dde8524291619c972256d39f2
RaonMaru <yyw10242@naver.com>
2021-02-01 16:45:53 +0900

    Step 3: Add Stateful Widget
    using hint "stful"
    making easy Stateful widget

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -15,9 +15,22 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
