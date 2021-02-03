052f17f8285c44a68d4b2d74cfd7f0d5b1121c59
RaonMaru <yyw10242@naver.com>
2021-02-03 18:02:50 +0900

    Bringing it all together
    but Still watching how it works..

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,145 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter Tutorial',
-      home: Counter(),
-    ),
-  );
+class Product {
+  const Product({this.name});
+  final String name;
 }
 
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({this.count});
+typedef void CartChangedCallback(Product product, bool inCart);
 
-  final int count;
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({this.product, this.inCart, this.onCartChanged})
+      : super(key: ObjectKey(product));
 
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
   }
-}
 
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({this.onPressed});
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
 
-  final VoidCallback onPressed;
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(
+        product.name,
+        style: _getTextStyle(context),
+      ),
     );
   }
 }
 
-//Changing widgets in response to input
-class Counter extends StatefulWidget {
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
+  final List<Product> products;
   @override
-  _CounterState createState() => _CounterState();
+  _ShoppingListState createState() => _ShoppingListState();
 }
 
-class _CounterState extends State<Counter> {
-  int _counter = 0;
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set<Product>();
 
-  void _increment() {
+  void _handleCartChanged(Product product, bool inCart) {
     setState(() {
-      //setState let flutter know somthing changed.
-      // so let flutter to update display values.
-      _counter++;
+      if (!inCart)
+        _shoppingCart.add(product);
+      else
+        _shoppingCart.remove(product);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CounterIncrementor(onPressed: _increment),
-        CounterDisplay(
-          count: _counter,
-        ),
-      ],
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('myButton was tapped!');
-      },
-      child: Container(
-        alignment: Alignment.centerLeft,
-        width: 200,
-        height: 200,
-        padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.green[200]),
-        child: Text('MyButtonBox'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
       ),
     );
   }
 }
 
-class TutorialHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: Text('Example title'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            ),
-            IconButton(
-              icon: Icon(Icons.add_call),
-              tooltip: 'Call',
-              onPressed: null,
-            )
-          ],
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.baby_changing_station),
-              ),
-              Tab(
-                icon: Icon(Icons.cached),
-              ),
-              Tab(
-                icon: Icon(Icons.dangerous),
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            Icon(Icons.baby_changing_station),
-            Icon(Icons.cached),
-            Wrap(
-              children: [Icon(Icons.dangerous), MyButton()],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          tooltip: 'Add',
-          child: Icon(Icons.add),
-          onPressed: null,
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Shopping App',
+      home: ShoppingList(
+        products: <Product>[
+          Product(name: 'Eggs'),
+          Product(name: 'Flour'),
+          Product(name: 'Chocolate chips'),
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
