b1f68a080ac23e118400a9d13c5ef6df683d7c54
RaonMaru <yyw10242@naver.com>
2021-02-03 14:55:50 +0900

    Tried DefaultTabController
    DefaultTabController
    TabBar on AppBar's bottom
    TabBarView on DefaultTabController's body

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -12,34 +12,55 @@ void main() {
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
             onPressed: null,
           ),
-          IconButton(
-            icon: Icon(Icons.add_call),
-            tooltip: 'Call',
-            onPressed: null,
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('Hello World'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
+          title: Text('Example title'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+            IconButton(
+              icon: Icon(Icons.add_call),
+              tooltip: 'Call',
+              onPressed: null,
+            )
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.baby_changing_station),
+              ),
+              Tab(
+                icon: Icon(Icons.cached),
+              ),
+              Tab(
+                icon: Icon(Icons.dangerous),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.baby_changing_station),
+            Icon(Icons.cached),
+            Icon(Icons.dangerous)
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Add',
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
       ),
     );
   }
