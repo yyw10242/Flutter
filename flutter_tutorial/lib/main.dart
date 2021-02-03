07737b4f88acdf5a3bade133d0241d5c83e9a8ab
RaonMaru <yyw10242@naver.com>
2021-02-03 13:15:39 +0900

    Using Material Components

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,69 +1,54 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
 
-  final Widget title;
-
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(color: Colors.blue, boxShadow: <BoxShadow>[
-        BoxShadow(
-            color: Colors.black26,
-            blurRadius: 30,
-            offset: Offset(5, 5),
-            spreadRadius: 2),
-      ]),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          Expanded(child: title),
+    return DefaultTabController(Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
             tooltip: 'Search',
             onPressed: null,
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
+          IconButton(
+            icon: Icon(Icons.add_call),
+            tooltip: 'Call',
+            onPressed: null,
           )
         ],
+        bottom: TabBar(
+          key: Key('tabBar'),
+          tabs: <Widget>[
+            Tab(text: 'a'),
+            Tab(text: 'b'),
+            Tab(text: 'c'),
+          ],
+        ),
       ),
-    );
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    ));
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    title: 'My app',
-    home: SafeArea(
-      child: MyScaffold(),
-    ),
-  ));
 }
