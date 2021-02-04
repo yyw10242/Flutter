af87bae3fcfe08d9d83f31f47550ec796c778acf
RaonMaru <yyw10242@naver.com>
2021-02-04 18:03:25 +0900

    Failed to Clone
    but let's move on

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -4,10 +4,15 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
     return MaterialApp(
       title: 'Flutter Layout Demo',
       theme: ThemeData(
@@ -29,70 +34,69 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(color: Colors.grey[500]),
-            ),
-          ],
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+            ],
+          ),
         ),
-      ),
-      Icon(Icons.star),
-      Text('41'),
-    ],
-  ),
-);
+        Icon(Icons.star),
+        Text('41'),
+      ],
+    ),
+  );
 
-Column _buildButtonColumn(Color color, IconData icon, String label) {
-  return Column(
-    children: [
-      Icon(
-        icon,
-        color: color,
-      ),
-      Text(label),
-    ],
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Text(label),
+      ],
+    );
+  }
+
+//failed here. i can't fide where the context came from so i don't know where this code go to.
+  Widget buttonSection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    ),
+  );
+
+  Widget textSection = Container(
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
+    ),
   );
 }
-
-Color color = Theme.of(context).primaryColor;
-
-Widget buttonSection = Container(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      _buildButtonColumn(color, Icons.call, 'CALL'),
-      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-      _buildButtonColumn(color, Icons.share, 'SHARE'),
-    ],
-  ),
-);
-
-Widget textSection = Container(
-  child: Text(
-    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-    'Alps. Situated 1,578 meters above sea level, it is one of the '
-    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-    'half-hour walk through pastures and pine forest, leads you to the '
-    'lake, which warms to 20 degrees Celsius in the summer. Activities '
-    'enjoyed here include rowing, and riding the summer toboggan run.',
-  ),
-);
